63848a42248ad4dccca2642101cfe02a2efb98a7
Baptiste Bucamp <baptiste.bucamp@epita.fr>
2023-06-07 00:23:20 +0200

    login page

--- src/front.py
+++ src/front.py
@@ -1,8 +1,6 @@
-from flask import Flask
+from flask import Flask,request,render_template,redirect,url_for
+from crypto import *
 # from flask_pymongo import pymongo
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 app = Flask(__name__)
 # app.secret_key = ""
@@ -18,7 +16,27 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello World!'
+    return "Welcome to the homepage!"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Perform login authentication
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Add your authentication logic here (e.g., check credentials against a database)
+        # For simplicity, let's assume the username is 'admin' and password is 'password'
+        if username == 'admin' and password == 'password':
+            return redirect(url_for('dashboard'))
+        else:
+            return "Invalid credentials. Please try again."
+    
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return "Welcome to the dashboard!"
 
 # @app.route('/delete/<username>', methods=['POST'])
 # def delete_entries(username):
@@ -26,3 +44,6 @@ def index():
 #     db.city_collection.delete_many({"username": username})
 #     flash("All entries have been deleted.")
 #     return redirect(url_for('visited', username=username))
+
+if __name__ == '__main__':
+    app.run(debug=True)
